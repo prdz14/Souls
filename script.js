8925b2fcfc5341328cb612dd9b57ff5804b687a8
AminKh <mihakh13@mail.ru>
2023-04-21 14:30:17 +0300

    discover-not-end

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    loopedSlides: null,
+    centeredSlides: true,
+    slidesPerView: 3,
+    keyboard: {
+        enabled: true,
+        pageUpDown: true
+    }
+});
