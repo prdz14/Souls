03ba7d735ccfaa3460a593c533a2be869fe0cd30
AminKh <mihakh13@mail.ru>
2023-04-22 23:14:30 +0300

    dicsover

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-    loopedSlides: null,
-    centeredSlides: true,
-    slidesPerView: 3,
-    keyboard: {
-        enabled: true,
-        pageUpDown: true
-    }
-});
+// const swiper = new Swiper(".swiper", {
+//   direction: "horizontal",
+//   loop: true,
+//   loopedSlides: null,
+//   centeredSlides: true,
+//   slidesPerView: 6,
+//   keyboard: {
+//     enabled: true,
+//     pageUpDown: true,
+//   },
+// //   autoplay: {
+// //     delay: 2000,
+// //   },
+//   onlyInViewport: true,
+//   draggable: true,
+//   spaceBetween: 30,
+// });
+
+// const swiper = new Swiper(".swiper", {
+//   direction: "horizontal",
+//   loop: true,
+
+//   keyboard: {
+//     enabled: true,
+//     pageUpDown: true,
+//     onlyInViewport: true,
+//   },
+
+//   mousewheel: true,
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 1000,
+//     stopOnLastSlide: true,
+//     disableOnInteraction: false,
+//   },
+//   speed: 1000,
+
+//   effect: "coverflow",
+//   coverflowEffect: {
+//     rotate: 20,
+//   },
+
+//   breakPoints: {
+//     320: {
+//       slidesPerView: 1,
+//     },
+//     400: {
+//       slidesPerView: 2,
+//     },
+//     992: {
+//       slidesPerView: 3,
+//     },
+//   },
+// });
